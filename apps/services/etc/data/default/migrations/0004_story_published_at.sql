178207537a43ebb1f8e49f1b2f2f2564a37cf932
Eser Ozvataf <eser@acikyazilim.com>
2026-01-20 05:03:03 +0300

    fix: fixed migration for published_at column in story table
    - Created a new SQL migration to add the published_at column to the story table, enabling tracking of publication timestamps.
    - Included rollback functionality to drop the published_at column if necessary.

--- apps/services/etc/data/default/migrations/0004_story_published_at.sql
+++ apps/services/etc/data/default/migrations/0004_story_published_at.sql
@@ -1,2 +1,8 @@
+-- +goose Up
+
 -- Add published_at column to story table
 ALTER TABLE "story" ADD COLUMN IF NOT EXISTS "published_at" TIMESTAMP WITH TIME ZONE;
+
+-- +goose Down
+
+ALTER TABLE "story" DROP COLUMN IF EXISTS "published_at";
