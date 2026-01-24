5f1f9cb12a7ae2dc2ee56ace22ebe4b84965488f
Eser Ozvataf <eser@acikyazilim.com>
2026-01-24 08:20:58 +0300

    fix: removed duplicate http logging.

--- apps/services/pkg/ajan/httpfx/middlewares/response_time_middleware.go
+++ apps/services/pkg/ajan/httpfx/middlewares/response_time_middleware.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"log"
 	"time"
 
 	"github.com/eser/aya.is/services/pkg/ajan/httpfx"
@@ -16,10 +15,7 @@ func ResponseTimeMiddleware() httpfx.Handler {
 		result := ctx.Next()
 
 		duration := time.Since(startTime)
-		// durationText := strconv.FormatInt(duration.Milliseconds(), 10) + "ms"
 		durationText := duration.String()
-
-		log.Println(ctx.Request.Method, ctx.Request.URL.Path, duration)
 
 		ctx.ResponseWriter.Header().Set(ResponseTimeHeader, durationText)
 
