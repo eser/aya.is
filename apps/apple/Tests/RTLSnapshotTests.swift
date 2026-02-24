5c30065a97c9884d29f3a2459200680f457a35d2
Alp Özcan <alpozcan.me@gmail.com>
2026-02-25 00:00:28 +0100

    fix: correct parameter order for image snapshot precision

--- apps/apple/Tests/RTLSnapshotTests.swift
+++ apps/apple/Tests/RTLSnapshotTests.swift
@@ -140,7 +140,7 @@ extension RTLSnapshotTests {
         let snapshotName = name.map { "\($0)-\(platformSuffix)" } ?? platformSuffix
         let vc = UIHostingController(rootView: localized)
         vc.view.frame = CGRect(x: 0, y: 0, width: 390, height: height)
-        assertSnapshot(of: vc, as: .image(precision: 1, perceptualPrecision: 0.98, on: .init(safeArea: .zero, size: CGSize(width: 390, height: height), traits: .init())), named: snapshotName, file: file, testName: testName, line: line)
+        assertSnapshot(of: vc, as: .image(on: .init(safeArea: .zero, size: CGSize(width: 390, height: height), traits: .init()), precision: 1, perceptualPrecision: 0.98), named: snapshotName, file: file, testName: testName, line: line)
         #elseif os(macOS)
         let platformSuffix = "macOS"
         let snapshotName = name.map { "\($0)-\(platformSuffix)" } ?? platformSuffix
