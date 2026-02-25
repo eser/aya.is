e23a8397cc9e54a979193d3cdcef459c30432fd0
Eser Ozvataf <eser@acikyazilim.com>
2026-02-25 23:06:08 +0300

    feat: localization improvements (take II).

--- apps/services/etc/data/default/migrations/0051_story_tx_is_managed.sql
+++ apps/services/etc/data/default/migrations/0051_story_tx_is_managed.sql
@@ -1,5 +1,9 @@
+-- +goose Up
 ALTER TABLE "story_tx" ADD COLUMN "is_managed" BOOLEAN DEFAULT FALSE NOT NULL;
 
 -- Mark all existing translations of managed stories as managed
 UPDATE "story_tx" SET is_managed = TRUE
 WHERE story_id IN (SELECT id FROM "story" WHERE is_managed = TRUE);
+
+-- +goose Down
+ALTER TABLE "story_tx" DROP COLUMN IF EXISTS "is_managed";
