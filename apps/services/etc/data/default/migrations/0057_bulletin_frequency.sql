ec0c490fcad335eb6799ce5db69deee1abfad6ae
Eser Ozvataf <eser@acikyazilim.com>
2026-02-28 01:39:26 +0300

    feat: bulletin system (take VII).

--- apps/services/etc/data/default/migrations/0057_bulletin_frequency.sql
+++ apps/services/etc/data/default/migrations/0057_bulletin_frequency.sql
@@ -1,6 +1,11 @@
+-- +goose Up
 -- Add configurable digest frequency to bulletin subscriptions.
 -- Values: 'daily' (default), 'bidaily' (every 2 days), 'weekly'.
 -- "Don't send" is represented by soft-deleting all subscriptions.
 ALTER TABLE "bulletin_subscription"
   ADD COLUMN "frequency" TEXT NOT NULL DEFAULT 'daily'
     CHECK (frequency IN ('daily', 'bidaily', 'weekly'));
+
+-- +goose Down
+ALTER TABLE "bulletin_subscription"
+  DROP COLUMN "frequency";
